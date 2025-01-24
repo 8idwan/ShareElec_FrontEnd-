98f67a5faa43f321107b346cdf07b7d82a7f0044
8idwan <ridouane.announ@gmail.com>
2025-01-24 20:33:57 +0100

    Fix SideBar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { OffreListComponent } from './offre/offre-list/offre-list.component';
 import { NgModule } from '@angular/core';
+import { MesOffresComponent } from './offre/mes-offres/mes-offres.component';
 
 
 export const routes: Routes = [ {path:'',component:OffreListComponent},
-  
+                                 {path:'mesoffres',component:MesOffresComponent},
   {
     path: 'offre',
     loadChildren: () => import('./offre/offre.module')
---
+++ src/app/offre/mes-offres/mes-offres.component.ts
@@ -0,0 +1,169 @@
+import { Component } from '@angular/core';
+import { Offre } from '../model/offre.model';
+import { CommonModule } from '@angular/common';
+import { AddOffreComponent } from '../add-offre/add-offre.component';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-mes-offres',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './mes-offres.component.html',
+  styleUrl: './mes-offres.component.css'
+})
+export class MesOffresComponent {
+  offers: Offre[] = [
+      new Offre(
+        '1',
+        'EDF Énergies',
+        'Professionnel',
+        5000,
+        '20-02-2025',
+        'Éolien',
+        0.12,
+        true,
+        'Disponible'
+      ),
+      new Offre(
+        '1',
+        'EDF Énergies',
+        'Professionnel',
+        5000,
+        '20-02-2025',
+        'Éolien',
+        0.12,
+        true,
+        'Disponible'
+      ),
+      new Offre(
+        '1',
+        'EDF Énergies',
+        'Professionnel',
+        5000,
+        '20-02-2025',
+        'Éolien',
+        0.12,
+        true,
+        'Disponible'
+      ),
+      new Offre(
+        '1',
+        'EDF Énergies',
+        'Professionnel',
+        5000,
+        '20-02-2025',
+        'Éolien',
+        0.12,
+        true,
+        'Disponible'
+      ),
+      new Offre(
+        '1',
+        'EDF Énergies',
+        'Professionnel',
+        5000,
+        '20-02-2025',
+        'Éolien',
+        0.12,
+        true,
+        'Disponible'
+      ),
+      new Offre(
+        '1',
+        'EDF Énergies',
+        'Professionnel',
+        5000,
+        '20-02-2025',
+        'Éolien',
+        0.12,
+        true,
+        'Disponible'
+      ),
+      new Offre(
+        '1',
+        'EDF Énergies',
+        'Professionnel',
+        5000,
+        '20-02-2025',
+        'Éolien',
+        0.12,
+        true,
+        'Disponible'
+      ),
+      new Offre(
+        '1',
+        'EDF Énergies',
+        'Professionnel',
+        1000,
+        '20-02-2025',
+        'Éolien',
+        0.12,
+        true,
+        'Disponible'
+      ),
+      new Offre(
+        '1',
+        'EDF Énergies',
+        'Professionnel',
+        1000,
+        '20-02-2025',
+        'Éolien',
+        0.12,
+        true,
+        'Indisponible'
+      ),
+    ];
+  
+    offersPerPage = 6;
+    currentPage = 1;
+    constructor(private dialog:MatDialog) { }
+    opendiag(): void {
+      console.log("ll")
+      this.dialog.open(AddOffreComponent);
+  
+    }
+  
+  
+    get paginatedOffers(): Offre[] {
+      const startIndex = (this.currentPage - 1) * this.offersPerPage;
+      const endIndex = startIndex + this.offersPerPage;
+      return this.offers.slice(startIndex, endIndex);
+    }
+  
+    get totalPages(): number {
+      return Math.ceil(this.offers.length / this.offersPerPage);
+    }
+  
+    changePage(direction: number): void {
+      if (
+        (direction === -1 && this.currentPage > 1) ||
+        (direction === 1 && this.currentPage < this.totalPages)
+      ) {
+        this.currentPage += direction;
+      }
+    }
+  
+    calculePrixMoyen(): number {
+      if (this.offers.length === 0) return 0;
+  
+      const totalPrice = this.offers.reduce((sum, offer) => sum + offer.prixKw, 0);
+      return Number((totalPrice / this.offers.length).toFixed(3));
+    }
+  
+    offreActives(): number {
+      if (this.offers.length === 0) return 0;
+      return this.offers.filter((offer) => offer.status === 'Disponible').length;
+  
+    }
+  
+    quantiteTotalDisponible(): number {
+      if (this.offers.length === 0) return 0;
+  
+      // Filtrer les offres avec le statut "Disponible" et sommer leurs quantités
+      return this.offers
+        .filter((offer) => offer.status === 'Disponible') // Garder seulement les offres disponibles
+        .reduce((total, offer) => total + (offer.quantite || 0), 0); // Somme des quantités
+    }
+  
+
+}
