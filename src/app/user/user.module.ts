52bab7fdc2bd2fbe531f86d56b8c499d29ee1eeb
Assil28 <assil.dkhil28@gmail.com>
2025-02-14 21:13:06 +0100

    UserRouting UserModule

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { VerifyEmailComponent } from './verify-email/verify-email.component';
+import { UserRoutingModule } from './user-routing.module';
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    SignupComponent,
+    LoginComponent,
+    ProfileComponent,
+    VerifyEmailComponent,
+    UserRoutingModule,
+    HttpClientModule
+  ],
+
+})
+export class UserModule { }
