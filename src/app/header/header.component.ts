4b2d98f11e830b816b19844fb63c6f92f9e1dd79
Assil28 <assil.dkhil28@gmail.com>
2025-02-14 21:18:02 +0100

    header update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,34 @@
+import { Component,Inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-
+import { PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  isLoggedIn(): boolean {
+    // Check if we're in the browser before using localStorage
+    if (isPlatformBrowser(this.platformId)) {
+      return !!localStorage.getItem('token');
+    }
+    return false;
+  }
+
+  logout(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem('token');
+      this.router.navigate(['/user/login']);
+    }
+  }
 
 }
