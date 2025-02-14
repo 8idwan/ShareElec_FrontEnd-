4f679e1c28d816d6670c6599eef278772ccd0004
Assil28 <assil.dkhil28@gmail.com>
2025-02-14 21:23:57 +0100

    add auth

---
+++ src/app/auth/auth.interceptor.ts
@@ -0,0 +1,34 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req);
+};
+// auth.interceptor.ts
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      // Clone the request to add the new header
+      request = request.clone({
+        setHeaders: {
+          Authorization: Bearer ${token}
+        }
+      });
+    }
+    return next.handle(request);
+  }
+}
