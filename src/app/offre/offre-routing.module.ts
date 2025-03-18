68eacb8b084d7fad2f54ccc3b475d4a82799e980
Assil28 <assil.dkhil28@gmail.com>
2025-03-18 22:06:24 +0100

    fix route offre/mesOffres

--- src/app/offre/offre-routing.module.ts
+++ src/app/offre/offre-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OffreListComponent } from './offre-list/offre-list.component';
+import { MesOffresComponent } from './mes-offres/mes-offres.component';
 
 const routes: Routes = [
   {path:"offres",component:OffreListComponent},
-  
+  {path:"mesoffres",component:MesOffresComponent},
   
 ];
 
