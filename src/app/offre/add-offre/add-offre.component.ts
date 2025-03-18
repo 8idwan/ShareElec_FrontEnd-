503116bc60467bae598674690bb71d66c4a1ca39
Assil28 <assil.dkhil28@gmail.com>
2025-03-18 21:13:55 +0100

    Verification de la valeur d'energie saisie par l'utilisateur pour poster un offre , il faut quelle sera < a la quantité trouvé dans sa balance

--- src/app/offre/add-offre/add-offre.component.ts
+++ src/app/offre/add-offre/add-offre.component.ts
@@ -1,30 +1,37 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 import { OffreService } from '../service/offre.service';
 
 import { Router } from '@angular/router';
+import { UserResponseModel } from '../../user/model/user-response.model';
 
 
 @Component({
   selector: 'app-add-offre',
   standalone: true,
   imports: [ReactiveFormsModule, CommonModule, MatDialogModule, HttpClientModule],
-  providers: [OffreService],
+  providers: [OffreService,MatSnackBar],
   templateUrl: './add-offre.component.html',
   styleUrl: './add-offre.component.css'
 })
-export class AddOffreComponent {
+export class AddOffreComponent implements OnInit{
   addform: FormGroup;
-  user: any;
+  currentUser!: UserResponseModel | null; 
+  balance:any;
+  
 
   constructor(
     private fb: FormBuilder,
     private offreservice: OffreService,
     private dialogRef: MatDialogRef<AddOffreComponent>,
-    private router: Router
+    private router: Router,
+    private dialog: MatDialog,  // Ajout du MatDialog
+  private snackBar: MatSnackBar,  // Ajout du SnackBar
 
   ) {
     this.addform = this.fb.group({
@@ -35,14 +42,44 @@ export class AddOffreComponent {
     });
   }
 
+    ngOnInit(): void {
+      // Load user from localStorage
+      const storedUserJson = localStorage.getItem('currentUser');
+      if (storedUserJson) {
+        this.currentUser = JSON.parse(storedUserJson) as UserResponseModel;
+      }
+      console.log(this.currentUser?.id);
+      console.log(this.currentUser?.sommeEnergie);
+
+      // Vérifier si sommeEnergie est disponible et mettre à jour la validation de quantite
+  if (this.currentUser?.sommeEnergie !== undefined) {
+    this.addform.get('quantite')?.setValidators([
+      Validators.required,
+      Validators.min(1),
+      Validators.max(this.currentUser.sommeEnergie)
+    ]);
+    this.addform.get('quantite')?.updateValueAndValidity();
+  }
+    }
+
   onSubmit() {
+    // Si l'utilisateur saisie une somme > a L'energie qu'il possede dans sa balance
+      const quantite = Number(this.addform.value.quantite);
+      if (this.currentUser && this.currentUser.sommeEnergie !== undefined) {
+        if (quantite > this.currentUser.sommeEnergie) {
+          this.showWarningMessage(this.currentUser.sommeEnergie);
+          return; 
+        }
+      }
+    /************************************************** */
+
     if (this.addform.valid) {
       const formData = {
         quantite: Number(this.addform.value.quantite),
         vendDetails: Boolean(this.addform.value.vendDetails),
         type: String(this.addform.value.type),
         prixKw: Number(this.addform.value.prixKw),
-        userId: 2,
+        userId:this.currentUser?.id,
         status: true, 
         date: new Date().toISOString().split('T')[0]
       };
@@ -74,4 +111,14 @@ export class AddOffreComponent {
   closeDialog() {
     this.dialogRef.close();
   }
+
+  showWarningMessage(balance:number) {
+    this.snackBar.open('Veuillez saisir une quantité inférieure à votre somme d\'énergie : ' + balance, 'Fermer', {
+      duration: 3000, // 3 secondes
+      panelClass: ['warning-snackbar'] 
+    });
+  }
+  
+
+  
 }
--- src/app/offre/offre.module.ts
+++ src/app/offre/offre.module.ts
@@ -5,6 +5,7 @@ import { OffreRoutingModule } from './offre-routing.module';
 import { AddOffreComponent } from './add-offre/add-offre.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
     AddOffreComponent,
     ReactiveFormsModule,
     FormsModule,
+    MatSnackBarModule
   ]
 })
 export class OffreModule { }
