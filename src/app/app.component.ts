aac3aaf74b2b5621e8f659c1c4840803905e6687
8idwan <ridouane.announ@gmail.com>
2025-03-20 15:51:01 +0100

    Errors Fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { OffreListComponent } from './offre/offre-list/offre-list.component';
 import { SideMenuComponent } from "./offre/side-menu/side-menu.component";
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -17,11 +18,20 @@ export class AppComponent implements OnInit {
   title = 'ShareElec';
   isAuthenticated: boolean = false;
 
+  constructor(private router: Router) {}
+
   ngOnInit() {
     this.checkAuthentication();
+
+    // Subscribe to router events to check authentication after navigation
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.checkAuthentication();
+    });
   }
 
   checkAuthentication() {
     this.isAuthenticated = !!localStorage.getItem('token');
-  }  
+  }
 }
